c78fa821a0205226529408a7234b9c081f874c2d
LouisaAndrade <lamodeandrade@gmail.com>
2025-08-11 20:48:37 -0300

    sa

--- pi_react/src/App.js
+++ pi_react/src/App.js
@@ -2,23 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let oi = "ola mundo";
+  oi+="Marcius;"
+  function soma (a, b){ // parametros
+    return a + b 
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      
+    {
+      (soma (oi, "Marcius"))
+    }
+      
+    </main>
   );
 }
 
