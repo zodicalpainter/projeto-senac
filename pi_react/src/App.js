f54a138b9cc3128e1de6b52eb8a42aeabead0ba4
LouisaAndrade <lamodeandrade@gmail.com>
2025-08-11 21:50:40 -0300

    fiz a cesta

--- pi_react/src/App.js
+++ pi_react/src/App.js
@@ -1,19 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  let oi = "ola mundo";
-  oi+="Marcius;"
-  function soma (a, b){ // parametros
-    return a + b 
-  }
-
-  return (
-    <main className="App">
-      
-    {
-      (soma (oi, "Marcius"))
+function App() { //aqui é java script
+  function calculadora ( a, b, op){
+    switch(op){
+      case '+':
+        return a + b 
+        case '-':
+          return a - b
+          case '*':
+            return a * b
+            case '/'
+            return a/ b
     }
+  }
+   
+  function calculadora (a, b, op) {
+    if(op == '+'){
+      return a + b 
+    }
+     else if (op == '-'){
+      return a - b;
+     }else if( op == '*'){
+      return a + b 
+     } else if (op == "/"){
+      return a/b 
+     }
+
+     }
+
+
+
+  return ( /* aqui é html */
+
+    <main className="App">
+      {calculadora ( 5, 5, '+') } </br>
+
       
     </main>
   );
