4e8bbd8dc30918d0972d1d0e47b63ee005bf1fa2
Hiaviel <jorgeluciano_12@hotmail.com>
2025-08-14 21:57:02 -0300

    criacao de login com auth do supabase para cadsatro de usuarios e mensagens de erro

--- pi_react/src/App.js
+++ pi_react/src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import { createClient } from "@supabase/supabase-js";
+
+
+const supabaseUrl="https://giydnknndvmyznfrwgeb.supabase.co"
+const supabaseKey="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdpeWRua25uZHZteXpuZnJ3Z2ViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQzNTIyMDQsImV4cCI6MjA2OTkyODIwNH0.0OsZXLH0Eawyfc37Ip8zruPUht8PL0EKSwP3xDokGX0"
+const supabase = createClient(supabaseUrl, supabaseKey);
+
 
 function App() { //aqui é java script
 
+  const [Msg, setMsg] = useState("");
+
   const [login, setlogin] = useState(true);
   const [user, setuser] = useState({
-    email:"",
-    senha:""
-  })
-
-  const[cadastro, setcadastro] = useState({
     name:"",
     email:"",
     password: "",
     phone:""
   })
 
+  const [IsSendRegister, SetSendRegister] = useState(false)
+
+  async function register(){
+
+    SetSendRegister(true);
+    console.log(user)
+      try{
+      let { data, error } = await supabase.auth.signUp({
+        email: user.email,
+        password: user.password
+      })
+
+      if(error) throw error
+
+      if(data.status == 400) throw data.message
+      setMsg("Cadastro Realizado!");
+    }catch(e){
+      setMsg(`Erro: ${e.message}`); 
+      }
+
+      SetSendRegister(false);
+      setTimeout(() => setMsg("") , 5000);
+    }
+  
   return ( /* aqui é html */
-    <main ClassName= "App">
-      
+  <main ClassName= "App">
+    
      
       <div className= "login-box"> 
 
@@ -28,9 +56,9 @@ function App() { //aqui é java script
           <h1>Login</h1>
 
             <p>E-mail:</p>
-          <input placeholder="Digite o e-mail do cadastro:" onchange = {(e) => setuser({...user, email: e.target.value})} />
+          <input placeholder="Digite o e-mail do cadastro:" onChange = {(e) => setuser({...user, email: e.target.value})} />
             <p>Senha:</p>
-          <input placeholder="Digite sua senha:" onchange = {(e) => setuser({...user, senha: e.target.value})}/>
+          <input placeholder="Digite sua senha:" onChange = {(e) => setuser({...user, password: e.target.value})}/>
           <br/><br/>
           <button className="btn">Entrar</button>
         </form>
@@ -40,16 +68,18 @@ function App() { //aqui é java script
           <h2>Cadastro:</h2>
 
           <p>Nome completo:</p>
-         
-          <input placeholder='Digite seu nome completo...' onchange = {(e) => setcadastro({...cadastro, name: e.target.value})}/>
+          <input placeholder='Digite seu nome completo...' onchange = {(e) => 
+            ({...user, name: e.target.value})}/>
             <p>Senha:</p>
-          <input placeholder='escolha sua senha...'onchange = {(e) => setcadastro({...cadastro, password: e.target.value})}/>
+          <input type="password" placeholder='escolha sua senha...' onChange = {(e) => setuser({...user, password: e.target.value})}/>
             <p>E-mail</p>
-          <input placeholder='digite o email para cadastro...'onchange = {(e) => setcadastro({...cadastro, email: e.target.value})}/>
+          <input placeholder='digite o email para cadastro...' onChange = {(e) => setuser({...user, email: e.target.value})}/>
             <p>Telefone:</p>
-          <input placeholder='digite um telefone valido...'onchange = {(e) => setcadastro({...cadastro, phone: e.target.value})}/>
+          <input placeholder='digite um telefone valido...' onChange = {(e) => setuser({...user, phone: e.target.value})}/>
 
-          <br/><br/><button className="btn">Cadastrar</button>
+          <br/><br/><button className="btn" onClick={register} disabled={IsSendRegister}>
+            {IsSendRegister ? "cadastrando..." : "Cadastrar"}
+            </button>
         </form>
       
       )} <br/>
@@ -62,7 +92,9 @@ function App() { //aqui é java script
 
 
       </div>
-    </main>
+    {Msg && (<div className= "toast">{Msg}</div>)}
+
+  </main>
   );   
 }
 
